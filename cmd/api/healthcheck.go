0cf4e05f60be24f997d523629a312da726d8718f
Paweł Siwoń <siwonpawel@gmail.com>
2022-12-06 10:49:33 +0100

    Improve healthcheck endpoint

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server encountered a problem and could not process your request.", http.StatusInternalServerError)
+		return
+	}
+
+	js = append(js, '\n')
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/siwonpawel/greenlight
 
 go 1.19
+
+require github.com/julienschmidt/httprouter v1.3.0
