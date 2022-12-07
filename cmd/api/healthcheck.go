4b88bde045767bb0e96fd07b57ee1a021ce7d18c
Paweł Siwoń <siwonpawel@gmail.com>
2022-12-07 18:54:14 +0100

    add error responses support

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,16 +6,15 @@ import (
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	env := envelope{
-		"status":      "available",
+		"status": "available",
 		"system_info": map[string]string{
 			"environment": app.config.env,
-			"version": version,
-		}, 
+			"version":     version,
+		},
 	}
 
 	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
-		app.logger.Print(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
