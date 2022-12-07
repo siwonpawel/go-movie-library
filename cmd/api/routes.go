edb62a63889371c05cb5e152fa4743f3c74122b3
Paweł Siwoń <siwonpawel@gmail.com>
2022-12-07 20:51:03 +0100

    better error handling

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,6 +13,7 @@ func (app *application) routes() *httprouter.Router {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthCheck", app.healthcheckHandler)
+
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 
