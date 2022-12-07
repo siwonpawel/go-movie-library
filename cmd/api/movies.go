1bb8adec12d9cd92d886029b99b2fae785db650f
Paweł Siwoń <siwonpawel@gmail.com>
2022-12-07 22:34:44 +0100

    add validation

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,12 +6,13 @@ import (
 	"time"
 
 	"github.com/siwonpawel/greenlight/internal/data"
+	"github.com/siwonpawel/greenlight/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
 		Title   string       `json:"title"`
-		Year    string       `json:"year"`
+		Year    int32        `json:"year"`
 		Runtime data.Runtime `json:"runtime"`
 		Genres  []string     `json:"genres"`
 	}
@@ -19,6 +20,19 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
