ae4743eb482d4c4732bd58c1699796ca2c75fbf8
Paweł Siwoń <siwonpawel@gmail.com>
2022-12-09 19:48:42 +0100

    database core

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	_ "github.com/lib/pq"
+	"github.com/siwonpawel/greenlight/internal/data"
 )
 
 const version = "1.0.0"
@@ -29,6 +30,7 @@ type config struct {
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -50,10 +52,12 @@ func main() {
 		logger.Fatal(err)
 	}
 	defer db.Close()
+	logger.Println("database connection pool established")
 
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
