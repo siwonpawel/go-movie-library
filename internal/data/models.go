11567ae9b26c26a42a9f94b21019da63066317a1
Paweł Siwoń <siwonpawel@gmail.com>
2022-12-12 17:40:36 +0100

    adds support for metadata

--- internal/data/models.go
+++ internal/data/models.go
@@ -13,7 +13,7 @@ var (
 type Models struct {
 	Movies interface {
 		Insert(movie *Movie) error
-		GetAll(title string, genres []string, filters Filters) ([]*Movie, error)
+		GetAll(title string, genres []string, filters Filters) ([]*Movie, Metadata, error)
 		Get(id int64) (*Movie, error)
 		Update(movie *Movie) error
 		Delete(id int64) error
