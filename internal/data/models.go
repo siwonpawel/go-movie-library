9e75b5453aef2fafe11021dd5c2d7e0a896ccc54
Paweł Siwoń <siwonpawel@gmail.com>
2022-12-12 01:33:26 +0100

    adds filtering of movies

--- internal/data/models.go
+++ internal/data/models.go
@@ -13,6 +13,7 @@ var (
 type Models struct {
 	Movies interface {
 		Insert(movie *Movie) error
+		GetAll(title string, genres []string, filters Filters) ([]*Movie, error)
 		Get(id int64) (*Movie, error)
 		Update(movie *Movie) error
 		Delete(id int64) error
