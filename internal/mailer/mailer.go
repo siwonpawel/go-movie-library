43f02660aad156f77b7d790fcf3f413b8077e03a
Paweł Siwoń <siwonpawel@gmail.com>
2022-12-13 23:25:21 +0100

    retry sending email

--- internal/mailer/mailer.go
+++ internal/mailer/mailer.go
@@ -58,9 +58,14 @@ func (m Mailer) Send(recipient, templateFile string, data any) error {
 	msg.SetBody("text/plain", plainBody.String())
 	msg.AddAlternative("text/html", htmlBody.String())
 
-	err = m.dialer.DialAndSend(msg)
-	if err != nil {
-		return err
+	for i := 1; i <= 3; i++ {
+		err = m.dialer.DialAndSend(msg)
+		if err == nil {
+			return nil
+		}
+
+		time.Sleep(500 * time.Millisecond)
 	}
-	return nil
+
+	return err
 }
