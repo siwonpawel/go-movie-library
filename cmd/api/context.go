a54db3b89ffb2219c04617c34244e3c159e92d0d
Paweł Siwoń <siwonpawel@gmail.com>
2022-12-14 17:52:06 +0100

    adds authorization

--- cmd/api/context.go
+++ cmd/api/context.go
@@ -15,3 +15,12 @@ func (app *application) contextSetUser(r *http.Request, user *data.User) *http.R
 	ctx := context.WithValue(r.Context(), userContextKey, user)
 	return r.WithContext(ctx)
 }
+
+func (app *application) contextGetUser(r *http.Request) *data.User {
+	user, ok := r.Context().Value(userContextKey).(*data.User)
+	if !ok {
+		panic("missing user value in request context")
+	}
+
+	return user
+}
