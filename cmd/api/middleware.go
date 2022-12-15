ed3714ee38ab5eb6ef460007bf88141a7fbb3571
Paweł Siwoń <siwonpawel@gmail.com>
2022-12-15 15:34:34 +0100

    adds unrestricted CORS

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -154,3 +154,11 @@ func (app *application) requireActivatedUser(next http.HandlerFunc) http.Handler
 
 	return app.requireAuthenticatedUser(fn)
 }
+
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		next.ServeHTTP(w, r)
+	})
+}
