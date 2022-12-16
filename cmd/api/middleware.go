591da4a9b49246fe0187ee6eef5213deb3635162
Paweł Siwoń <siwonpawel@gmail.com>
2022-12-16 16:29:02 +0100

    adds capturing response code

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
 
+	"github.com/felixge/httpsnoop"
 	"github.com/siwonpawel/greenlight/internal/data"
 	"github.com/siwonpawel/greenlight/internal/validator"
 	"golang.org/x/time/rate"
@@ -192,15 +194,19 @@ func (app *application) metrics(next http.Handler) http.Handler {
 	totalResponsesSent := expvar.NewInt("total_responses_sent")
 	totalProcessingTimeMicroseconds := expvar.NewInt("total_processing_time_μs")
 
+	totalResponsesSentByStatus := expvar.NewMap("total_responses_sent_by_status")
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		totalRequestsReceived.Add(1)
 
-		next.ServeHTTP(w, r)
+		metrics := httpsnoop.CaptureMetrics(next, w, r)
 
 		totalResponsesSent.Add(1)
 
 		duration := time.Since(start).Microseconds()
 		totalProcessingTimeMicroseconds.Add(duration)
+
+		totalResponsesSentByStatus.Add(strconv.Itoa(metrics.Code), 1)
 	})
 }
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require github.com/lib/pq v1.10.2
 require golang.org/x/time v0.3.0
 
 require (
+	github.com/felixge/httpsnoop v1.0.2
 	github.com/go-mail/mail/v2 v2.3.0
 	golang.org/x/crypto v0.4.0
 )
